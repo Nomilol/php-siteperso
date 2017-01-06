d169f73e5b61ea2d97a2118b797599a1eafddf0d
Axel <lezinaxel@gmail.com>
2017-01-06 10:12:54 +0100

    Inclusion du menu dans le document header.php

--- parts/header.php
+++ parts/header.php
@@ -1,1 +1,14 @@
-<div class="header">Bienvenue sur mon site</div>
+<?php 
+include "menu.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="../public/style.css">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
