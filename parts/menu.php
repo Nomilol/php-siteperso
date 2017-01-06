a6c4331aabad4cfb34e1a835f2d6db6aab9478a1
Axel <lezinaxel@gmail.com>
2017-01-06 11:14:55 +0100

    Suppression des balises inutiles

--- parts/menu.php
+++ parts/menu.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	<div class="menu">
-		<ul>
-			<li><a href="home.php">Accueil</a></li>
-			<li><a href="contact.php">Contact</a></li>
-			<li><a href="bio.php">Bio</a></li>
-		</ul>
-	</div>
-</body>
-</html>
+<div class="menu">
+	<ul>
+		<li><a href="index.php">Accueil</a></li>
+		<li><a href="index.php?p=contact">Contact</a></li>
+		<li><a href="index.php?p=bio">Bio</a></li>
+	</ul>
+	
